1d2f2d271ccda0fe03fa65f6cf99560bd9b87d59
Nur Rozalina <nur.rozalina@gmail.com>
2014-08-15 12:37:49 +0800

    upto relational database. added rails g migration add_user_id_to_statuses, edited db-migration-xx file, cleared console

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook3::Application.config.secret_token = 'd1938f60ad1d3ee5ae71b2ad8f34f457235bca220b06477d37d26ae3076914f19647cef76535e0a86f70b1008564e04f69a6b1ac6139351255f728a68a58e005'
